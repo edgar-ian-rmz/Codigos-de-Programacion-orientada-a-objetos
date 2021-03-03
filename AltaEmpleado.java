419d78930228dbcac6bb71b6a41266d362d4ad2a
Ramírez López Edgar Ian <72110019+edgar-ian-rmz@users.noreply.github.com>
2021-03-02 23:07:26 -0600

    Add files via upload

--- AltaEmpleado.java
+++ AltaEmpleado.java
@@ -375,8 +375,18 @@ public class AltaEmpleado extends javax.swing.JFrame {
          emp.setApmaterno( jTextField3.getText() );
          emp.setEdad( Integer.parseInt ( jTextField4.getText()) );
          emp.setCurp ( jTextField5.getText());
+         emp.setDomicilio(new Direccion());
+         emp.setDomicilio().setCalle(this.jTextField6.getText());
+         emp.getDomicilio().setNumero(this.jTextField7.getText());
+         emp.getDomicilio().setColonia(this.jTextField8.getText());
+         emp.getDomicilio().setDelegacion(this.jTextField9.getText());
+         emp.getDomicilio().setEstado(this.jTextField10.getText());
+         emp.setNumeroEmpleado(Integer.parseInt(this.jTextField11.getText()));
+         emp.setDepartemento(this.jTextField12.getText);
+         emp.setSueldo(Float.parseFloat(this.jTextFiedl13.getText()));
+         emp.setHorasExtra(Integer.parseInt(this.jTextField14.getText()));
+         int confirmar = J0optionPane.showConfirmDialog(this, "¿Deseas guardar estos datos?");
          
-                
          
     }//GEN-LAST:event_jButton5MouseClicked
 private void limpiarFormulario(){
