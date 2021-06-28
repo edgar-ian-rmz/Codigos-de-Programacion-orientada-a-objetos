e1875ccd7a93a64c07537f356697c59b609bff6a
Ramírez López Edgar Ian <72110019+edgar-ian-rmz@users.noreply.github.com>
2021-06-28 17:09:28 -0500

    Add files via upload

---
+++ Circulo.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes.geometria;
+
+/**
+ *
+ * @author Puig
+ */
+public class Circulo implements Figura {
+     private float radio;
+
+    public Circulo() {
+    }
+
+    public Circulo(float radio) {
+        this.radio = radio;
+    }
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo{" + "radio=" + radio + '}';
+    }
+
+    @Override
+    public void area() {
+        float areaTemp=(float)Math.PI*(this.radio * this.radio);
+        System.out.println("El area del circulo es:" + areaTemp + "Mts." );
+        
+    }
+    
+}
